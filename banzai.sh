e8856ef450a2f48b992dc53b06c86ff4ecc7c26d
albihasani94 <albihasani94@gmail.com>
2019-12-30 18:03:04 +0100

    Add descriptive messages

--- banzai.sh
+++ banzai.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 # Clean
+echo "=====cleaning up====="
+echo "rm -rf classes mods/play.with.records.jar"
 rm -rf classes mods/play.with.records.jar
+echo "=====cleaning up finished====="
 
 # Compile
+echo "=====compiling====="
+echo "javac --enable-preview --release 14 --module-path mods -d classes --module-source-path src $(find src -name "*.java")"
 javac --enable-preview --release 14 --module-path mods -d classes --module-source-path src $(find src -name "*.java")
+echo "=====compiling finished====="
 
 # Package
+echo "=====packaging====="
+echo "jar --create --file=mods/play.with.records.jar --main-class=play.with.records.Main -C classes/play.with.records ."
 jar --create --file=mods/play.with.records.jar --main-class=play.with.records.Main -C classes/play.with.records .
+echo "=====packaging finished====="
 
 # Run
+echo "=====running====="
+echo "java --enable-preview --module-path mods --module play.with.records"
 java --enable-preview --module-path mods --module play.with.records
+echo "=====running finished====="
